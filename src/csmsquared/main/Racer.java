cdce282fe2bd358c235813c574eedb48e5dec4eb
Mclaug54 <mclaug54@uwm.edu>
2015-02-19 13:04:46 -0600

    Racer Class updated

---
+++ src/csmsquared/main/Racer.java
@@ -0,0 +1,36 @@
+package csmsquared.main;
+
+
+public class Racer 
+{
+	private int id;
+	private long startTime;
+	private long endTime;
+	
+	public Racer(int id)
+	{
+		this.id = id;
+	}
+	
+	public void start()
+	{
+		startTime = System.currentTimeMillis();
+	}
+	public void end()
+	{
+		endTime = System.currentTimeMillis();
+	}
+	public long elapsedTime()
+	{
+		return endTime-startTime;
+	}
+	public String toString()
+	{
+		return Time.toString(elapsedTime());
+	}
+	public int getId()
+	{
+		return id;
+	}
+	
+}

2c66e5d935d7c074fe0090db4e6207372d80bd5f
cjmowen <cjmowen@uwm.edu>
2015-02-23 15:39:29 -0600

    Cleaned up and fixed ChronoTimer. Fixed an error with printing a run while a race was ongoing.

--- src/csmsquared/main/ChronoTimer.java
+++ src/csmsquared/main/ChronoTimer.java
@@ -1,28 +1,35 @@
 package csmsquared.main;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.NoSuchElementException;
+import java.util.Queue;
+
+import csmsquared.channel.Channel;
 
 
-public class ChronoTimer {
-	private ArrayList<Run> runs;
-//	private ArrayList<Channel> channels;
-	private ArrayList<Racer> racer;
-	private boolean currentRun;
-	private int currentIndex;
-	private Racer currentRacer;
-	private boolean start;
+public class ChronoTimer
+{
+	private static int NUM_CHANNELS = 2; // The number of channels
+	
+	private ArrayList<Channel> channels;
+	private LinkedList<Run> runs;
+	private Queue<Racer> racerQueue;
+	
+	private boolean runExists;    // True if a run is currently going on
+	private Racer currentRacer;   // The racer that is currently being timed
+	
 	
 	/**
 	 * Initializes All the local variable.
 	 */
 	public ChronoTimer()
 	{
-		currentRun = true;
-		start = false;
-		currentIndex = -1;
-		runs = new ArrayList<Run>();
-		racer = new ArrayList<Racer>();
+		channels = new ArrayList<Channel>(NUM_CHANNELS);
+		runs = new LinkedList<Run>();
+		racerQueue = new LinkedList<Racer>();
+		
+		runExists = false;
 	}
 	
 	
@@ -32,68 +39,62 @@ public class ChronoTimer {
 	 */
 	public void num(int r)
 	{
-		racer.add(new Racer(r));
+		racerQueue.add(new Racer(r));
 	}
+	
 	
 	/**
-	 * newRun() Will start a new Run.
+	 * Creates a new run
 	 * @exception IllegalStateException - if Run is already ON.
 	 */
-	
 	public void newRun()
 	{
+		if(runExists) throw new IllegalStateException("Must End the previous Run");
 		
-		if(!currentRun) throw new IllegalStateException("Must End the previous Run");
-		currentIndex++;
 		runs.add(new Run());
-	
-		currentRun = false;
+		runExists = true;
 	}
+	
 	
 	/**
 	 * endRun will End the Run.
 	 * @exception IllegalStateException - Run must have started.
 	 */
-	
 	public void endRun()
 	{
-		if(currentRun!=false) throw new IllegalStateException("Must start new Run");
-		currentRun = true;
+		if(!runExists) throw new IllegalStateException("Must start new Run");
+		runExists = false;
 	}
+	
 	
 	/**
 	 * start will take one racer out of the Racer list and start its timing.
 	 * @exception IllegalStateException - If run has not started. If previous racer has not finished. And if There are no racer left in Racer List.
 	 */
-	
-	public void start(){
+	public void start()
+	{
+		if(!runExists) throw new IllegalStateException("Must start the Run");
+		if(currentRacer != null) throw new IllegalStateException("Must Stop previous racer");
+		if(racerQueue.isEmpty()) throw new IllegalStateException("There are no racer in the queue");
 		
-		if(currentRun) throw new IllegalStateException("Must start the Run");
-		if(start) throw new IllegalStateException("Must Stop previous racer");
-		
-		if(racer.isEmpty())
-			throw new IllegalStateException("There are no racer in the queue");
-		
-		currentRacer=racer.get(0);
+		currentRacer = racerQueue.poll();
 		currentRacer.start();
-
-		if(runs.get(currentIndex).hasRacer(currentRacer))
-			throw new IllegalStateException("Racer as already ran in the same Race. Can not run twice");
-		
-		racer.remove(0);
-		start = true;
 		
 	}
+	
+	
 	/**
 	 * stop will stop the the current Race. And it will also record players ending time and adds racer to a appropriate run.
 	 * @exception IllegalStateException if racer has not started yet.
 	 */
 	public void stop()
 	{
-		if(!start) throw new IllegalStateException("Must start before you Stop");
+		if(currentRacer == null) throw new IllegalStateException("Must start before you Stop");
+		
 		currentRacer.end();
-		runs.get(currentIndex).addRacer(currentRacer);
-		start = false;
+		runs.peekLast().addRacer(currentRacer);
+		
+		currentRacer = null;
 	}
 	/**
 	 * Print will print out all the racer's list in given run.
@@ -101,12 +102,11 @@ public class ChronoTimer {
 	 * @param Run - Integer Given RUN
 	 */
 	
+	
 	public void print(int Run)
 	{
 		Run = Run-1;
 		if(Run >= runs.size() || Run < 0) throw new NoSuchElementException("There No Run # "+(Run+1)+" found");
 		System.out.print(runs.get(Run).toString());
 	}
-	
-	
 }

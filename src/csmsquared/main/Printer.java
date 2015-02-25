66bb1e130b721c0c0d5b536b6df9e74f03ebee34
Sagar <Sagar@Sagar-PC>
2015-02-24 21:51:08 -0600

    Printer class is updated

--- src/csmsquared/main/Printer.java
+++ src/csmsquared/main/Printer.java
@@ -1,9 +1,14 @@
 package csmsquared.main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Printer {
 	// If output is null, print to stdout
 	private static String output = null;
-	
+	private static PrintWriter pw;
 	
 	/**
 	 * Prints the given string to the printer's set output
@@ -15,11 +20,24 @@ public class Printer {
 		}
 		else{
 			// TODO: print to the output file
+			pw.write(str);
+			pw.close();
+			
 		}
 	}
 	
 	
-	public static void setOutput(String output){
+	public static void setOutput(String o){
 		// Set the output file to the specified output
+	output = o;
+	try {
+		 pw = new PrintWriter(new File(output));
+	} catch (FileNotFoundException e) {
+		System.out.print(e.getMessage());
 	}
+	}
+	
+	
+	
+	
 }

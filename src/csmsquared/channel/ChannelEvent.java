2d81e4bf6b87ea96785cdfbd17d97a01e8e434df
cjmowen <cjmowen@uwm.edu>
2015-05-05 23:01:12 -0500

    Final commit.

--- src/csmsquared/channel/ChannelEvent.java
+++ src/csmsquared/channel/ChannelEvent.java
@@ -1,7 +1,7 @@
 package csmsquared.channel;
 
 public class ChannelEvent {
-	int origin;
+	int origin;	// The channel number that fired the event
 
 	public ChannelEvent(int origin){
 		this.origin = origin;

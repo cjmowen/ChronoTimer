ec3c55d8ab7d37ccd2a1a75ba20972ed0f559dc2
cjmowen <cjmowen@uwm.edu>
2015-05-10 20:46:28 -0500

    Finished

---
+++ src/csmsquared/test/UITest.java
@@ -0,0 +1,10 @@
+package csmsquared.test;
+
+import csmsquared.main.ChronoTimerUI;
+
+public class UITest {
+	public static void main(String[] args) {
+		ChronoTimerUI ui = new ChronoTimerUI();
+		ui.testUI();
+	}
+}
